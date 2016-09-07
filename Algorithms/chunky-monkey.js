9fffbe774ef4f69849783ad37bd98158dc0aaf05
jeevanjayaprakash <jeevan55@gmail.com>
2016-09-07 16:06:13 +0100

    chunky-monkey.js

---
+++ Algorithms/chunky-monkey.js
@@ -0,0 +1,24 @@
+/**
+ * Chunky Monkey
+ *
+ * Jeevan Jayaprakash
+ * jeevan.jayaprakash@cantab.net
+ *
+ * A function which splits an array (first argument) into groups the size (second argument) and returns them as a two-dimensional array. 
+ */
+ 
+function chunkArrayInGroups(arr, size) {
+  newArray = [];
+  var numOfArrays = Math.ceil(arr.length/size);
+  var start = 0;
+  var end = size;
+  
+  for(var i = 0; i < numOfArrays; i++) {
+
+  newArray[i] = arr.slice(start, end);
+  start += size;
+  end += size; // if end exceeds arr.length then end = array.length
+  }
+  
+  return newArray;
+}
